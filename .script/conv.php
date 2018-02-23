99d65aebcd875940838d7c76e53757c7ff4e99f9
wangxiaochi <wangxiaochi@tuhu.cn>
2018-02-23 14:06:12 +0800

    to md

--- .script/conv.php
+++ .script/conv.php
@@ -10,10 +10,14 @@ function do_dir($dir)
   while (false !== ($entry = $d->read())) {
     echo $entry."\n";
     if ($entry[0] === ".") continue;
-    if (is_dir($entry)) do_dir($entry);
-    if (is_file($entry)) {
-      echo "iconv $entry\n";
-      file_put_contents($entry, iconv("GBK", "UTF-8", file_get_contents($entry)));
+    $f = "$dir/$entry";
+    if (is_dir($f)) do_dir($f);
+    if (is_file($f)) {
+      $content = file_get_contents($f);
+      $md_content = preg_replace('/\n/', "\n\n", $content);
+      $md_file = substr($f,0,strlen($f)-4).".md";
+      echo "iconv $f => $md_file\n";
+      file_put_contents($md_file, iconv("GBK", "UTF-8", $md_content));
     }
   }
   $d->close();
